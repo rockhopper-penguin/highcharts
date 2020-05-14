c3ca23c958bd51243ebcec1781c470f9422ec92d
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-05-14 21:51:01 +0900

    グラフの表示

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <div id="app">
-  </div>
+	<div id="app">
+		<Graph></Graph>
+	</div>
 </template>
 
 <script lang="ts">
+	import { Component, Vue } from 'vue-property-decorator';
+	import Graph from '@/components/Graph.vue';
 
+	@Component ({
+		components: {
+			Graph,
+		},
+	})
+	export default class App extends Vue {
+	}
 </script>
 
 <style>
---
+++ src/Highcharts.ts
@@ -0,0 +1,4 @@
+import { Vue } from 'vue-property-decorator';
+import HighchartsVue from "highcharts-vue";
+
+Vue.use(HighchartsVue)
