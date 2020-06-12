564ef5161c5cd2516c957c8a89f12b627cfeb1a5
Rockhopper-Penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-06-12 22:17:19 +0900

    不要なファイルの削除

---
+++ highcharts/src/components/Graph.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Graph extends Vue {
+}
+</script>
+
+<style>
+</style>
