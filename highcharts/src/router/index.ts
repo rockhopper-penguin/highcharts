06aba2279aebcd5760d5f7aac14b25750fffcf6d
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-06-12 23:12:07 +0900

    bootstrapの追加

--- highcharts/src/router/index.ts
+++ highcharts/src/router/index.ts
@@ -1,8 +1,12 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Graph from "@/components/Graph.vue";
+import { BootstrapVue } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.use(VueRouter);
+Vue.use(BootstrapVue);
 
 const routes: Array<RouteConfig> = [
   {
